bf8af6964efa22a67887507b342f016632f4d97d
debbieisfree19 <tranthaotrang1608la@gmail.com>
2025-05-30 23:28:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -219,5 +219,8 @@ def account():
 def language():
     return render_template('language.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Lấy port từ biến môi trường (Render sẽ set biến PORT)
+    app.run(host="0.0.0.0", port=port, debug=True)
